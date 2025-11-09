799e46793d660134369efaa7eac3d8453f7015e5
Andrea Klost <anklost@siue.edu>
2025-11-08 18:36:01 -0600

    Added system for itemspawners

---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour {
+    [Header("Item Settings")]
+    [Tooltip("Prefab of the item that the ItemSpawner spawns")]
+    public GameObject itemPrefab;
+    [Tooltip("Location at which items are spawned")]
+    public Transform spawnPoint;
+    [Tooltip("Max number of items that can be concurrently spawned." +
+             "If the spawner attempts to spawn an item at this limit, the oldest spawned item will be destroyed.")]
+    public int maxItemInstances = 3;
+    [Tooltip("Number of seconds after an item is taken to spawn a new item")]
+    public float itemSpawnDelay = 3f;
+    
+    // Keeps track of currently spawned in items
+    private Queue<GameObject> _itemInstances;
+    // Keeps track of item currently occupying the spawner
+    private GameObject _occupyingItem;
+    
+    void Awake() {
+        // If spawnPoint is not set, default it to the gameObject's transform
+        if (spawnPoint == null) {
+            spawnPoint = gameObject.transform;
+        }
+
+        _itemInstances = new Queue<GameObject>();
+        _occupyingItem = null;
+    }
+
+    void Start() {
+        SpawnItem();
+    }
+
+    void SpawnItem() {
+        GameObject newItem = Instantiate(itemPrefab);
+        newItem.transform.position = spawnPoint.position;
+        _itemInstances.Enqueue(newItem);
+        _occupyingItem = newItem;
+        // If spawning a newItem causes there to be more instances than allowed, destroy the oldest instance
+        if (_itemInstances.Count > maxItemInstances) {
+            // Note: since Cat holds references to grabbable items, this may cause a missing reference
+            //       if the cat is holding the item or the item is a potential grab target
+            Destroy(_itemInstances.Dequeue());
+        }
+    }
+    
+    void OnTriggerExit(Collider other) {
+        // If item occupying the spawner leaves, spawn a new item after a delay
+        if (other.gameObject == _occupyingItem) {
+            _occupyingItem = null;
+            Invoke(nameof(SpawnItem), itemSpawnDelay);
+        }
+    }
+}
