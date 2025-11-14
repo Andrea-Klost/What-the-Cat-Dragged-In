4318af2da40915c2e3f68ec2b65d28d56755ad51
taitt-e <123587963+taitt-e@users.noreply.github.com>
2025-11-13 21:30:26 -0600

    Created empty functions for collision handling

--- Assets/Scripts/CauldronBrewing.cs
+++ Assets/Scripts/CauldronBrewing.cs
@@ -15,36 +15,6 @@ public class CauldronBrewing : MonoBehaviour
     public Slot resultSlot;
     public int currIndex = -1;
 
-    // Code is based on this Tutorial : https://youtu.be/1fbd-yTcMgY 
-    //Need an OnColl Script here for an Item, place into slots up to index 2
-    //If over index 2, make a popup saying "The Cauldron is Full!!"
-    void OnCollisionEnter(Collision coll){
-        // Find out what collided with the Cauldron
-
-        //If an Ingredient, handle as follows.
-        //Insert gameobj into slot as a sprite.
-        if ((coll.gameObject.layer == LayerMask.NameToLayer("Ingredient")) && (currIndex < 3))
-        {
-            //Increase currIndex
-            currIndex += 1;
-            //Setup nextSlot
-            Slot nextSlot = null;
-            nextSlot.index = currIndex;
-
-            //Setup currItem
-            currItem.itemName = coll.gameObject.tag;
-            //Might need a sprite component?
-            nextSlot.GetComponent<Image>().sprite = currItem.GetComponent<Image>().sprite;
-            nextSlot.item = currItem;
-            itemList[nextSlot.index] = currItem;
-            currItem = null;
-            //Destroy currItem gameObj.
-            Destroy(coll.gameObject);
-        }
-    }
-    //Need an OnColl Check for the player, make the UI popup if the player is colliding
-    //with Cauldron radius
-
     //Need a script to cancel, removing all Items, tutorial/instruction manual
     //will explain that removing all items will destroy them.
 
@@ -96,5 +66,51 @@ public class CauldronBrewing : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // Code is based on this Tutorial : https://youtu.be/1fbd-yTcMgY 
+    //Need an OnColl Script here for an Item, place into slots up to index 2
+    //If over index 2, make a popup saying "The Cauldron is Full!!"
+    void OnCollisionEnter(Collision coll){
+        // Find out what collided with the Cauldron
+
+        //If an Ingredient, handle as follows.
+        //Insert gameobj into slot as a sprite.
+        if ((coll.gameObject.layer == LayerMask.NameToLayer("Ingredient")) && (currIndex < 3))
+        {
+            //Increase currIndex
+            currIndex += 1;
+            //Setup nextSlot
+            Slot nextSlot = null;
+            nextSlot.index = currIndex;
+
+            //Setup currItem
+            currItem.itemName = coll.gameObject.tag;
+            //Might need a sprite component?
+            nextSlot.GetComponent<Image>().sprite = currItem.GetComponent<Image>().sprite;
+            nextSlot.item = currItem;
+            itemList[nextSlot.index] = currItem;
+            currItem = null;
+            //Destroy currItem gameObj.
+            Destroy(coll.gameObject);
+        }
+
+        //If Player Character, activate popup hoverover ui
+        //
+        if(coll.gameObject.layer == LayerMask.NameToLayer("Cat"))
+        {
+            
+        }
+    }
+    //Need an OnColl Check for the player, make the UI popup if the player is colliding
+    //with Cauldron radius
+
+    void OnCollisionExit(Collision coll)
+    {
+        //If Cat exits, deactivate popup hoverover ui
+        if(coll.gameObject.layer == LayerMask.NameToLayer("Cat"))
+        {
+            
+        }
     }
 }
