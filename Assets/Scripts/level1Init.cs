56e1d29908d8ba6128d3b853cc196c83e9b5dd02
BrandonWaffle <bhaynes207371@gmail.com>
2025-11-15 12:10:37 -0600

    weird
    weird

---
+++ Assets/Scripts/level1Init.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Level1Init : MonoBehaviour
+{
+    void Start()
+    {
+        Time.timeScale = 1f;  // makes sure that you can actually play the game in once you click level 1
+        
+    }
+}
